b6e60b5e72aedb3bf1dbf70f3cbf96179130e389
leobtt <leonarbat8@gmail.com>
2022-03-30 08:26:09 -0300

    fake API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { createServer } from 'miragejs'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +9,20 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('transactions', () => {
+      return {
+        id: 1,
+        title: 'transaction 1',
+        category: 'development',
+        amout: 400,
+        type: 'deposit',
+        createdAt: new Date().getTime(),
+      }
+    })
+  },
+})
