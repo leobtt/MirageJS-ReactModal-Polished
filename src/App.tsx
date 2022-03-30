e50909e49ef0c8ca9ec310f06a4594b2307cb3ca
leobtt <leonarbat8@gmail.com>
2022-03-30 08:26:45 -0300

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
+import Modal from 'react-modal'
+import { NewTransactionModal } from './components/NewTransactionModal'
 
 export function App() {
+  const [newTransactionModal, setNewTransactionModal] = useState(false)
+
+  function openNewTransactionModal() {
+    setNewTransactionModal(true)
+  }
+
+  function closeNewTransactionModal() {
+    setNewTransactionModal(false)
+  }
+
   return (
     <React.Fragment>
-      <Header />
+      <Header onOpenNewTransactionModal={openNewTransactionModal} />
+      <NewTransactionModal isOpen={newTransactionModal} onRequestClose={closeNewTransactionModal} />
       <Dashboard />
       <GlobalStyle />
     </React.Fragment>
